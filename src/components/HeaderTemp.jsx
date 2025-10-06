d5d47ad7941645ad0a1d8f999e564d44bb7e418f
Dias Sunnatilla <diassunnatilla@MacBook-Air-Dias.local>
2025-10-06 11:16:52 +0500

    header navbar

--- src/components/HeaderTemp.jsx
+++ src/components/HeaderTemp.jsx
@@ -1,17 +1,38 @@
 
-
+import logo from '../img/Logo.svg';
+import search from '../img/search.svg';
+import cart from '../img/cart.svg';
+import '../App.css';
 function Header() {
     return (
         <header>
-            <h2>Orgamick</h2>
-            <nav>
+            <nav className="navbar">
+                <div className="logo">
+                    <img src={logo} alt="logo" />
+                    Organick
+                </div>
                 <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#services">Services</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Pages <span></span></li>
+                    <li>Shop</li>
+                    <li>Projects</li>
+                    <li>News</li>
                 </ul>
+                <div className="buttons">
+                    <div className="searchInput">
+                        <input type="text"/>
+                        <button className='searchBtn'><img src={search} alt="search" /></button>
+                    </div>
+                    <button className="cart">
+                        <div className="cartBtn">
+                            <img src={cart} alt="cart" />
+                        </div>
+                        <p>Cart (0)</p>
+                    </button>
+                </div>
             </nav>
+
         </header>
     )
 }
