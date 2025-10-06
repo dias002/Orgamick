90a022830156cf74069d51eb168e3382c661cabc
dias002 <dias.sun666@gmail.com>
2025-10-06 15:08:51 +0500

    first commit

---
+++ src/components/FooterTemp.jsx
@@ -0,0 +1,10 @@
+
+
+function Footer(){
+    return(
+        <footer>
+            <p>© 2024 Orgamick. All rights reserved.</p>
+        </footer>
+    )   
+}
+export default Footer;
---
+++ src/components/HeaderTemp.jsx
@@ -0,0 +1,48 @@
+
+import logo from '../img/Logo.svg';
+import search from '../img/search.svg';
+import cart from '../img/cart.svg';
+import '../App.css';
+function Header() {
+    return (
+        <header>
+            <nav className="navbar">
+                <div className="logo">
+                    <img src={logo} alt="logo" />
+                    Organick
+                </div>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Pages <span></span></li>
+                    <li>Shop</li>
+                    <li>Projects</li>
+                    <li>News</li>
+                </ul>
+                    
+                <div className="buttons">
+                    <div className="searchInput">
+                        <input type="text"/>
+                        <button className='searchBtn'><img src={search} alt="search" /></button>
+                    </div>
+                    <button className="cart">
+                        <div className="cartBtn">
+                            <img src={cart} alt="cart" />
+                        </div>
+                        <p>Cart (0)</p>
+                    </button>
+                </div>
+            </nav>
+            <div className="header-banner">
+                    <div className="bannerTitle">
+                        <h3>100% Natural Food</h3>
+                        <h1>Choose the best <br/>
+                            healthier way<br/>
+                            of life</h1>
+                        <button>Explore Now</button>
+                    </div>
+            </div>
+        </header>
+    )
+}
+export default Header;
