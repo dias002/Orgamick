1997e3ae64af7ec772be91dbd023a1f2e7918966
Dias Sunnatilla <diassunnatilla@MacBook-Air-Dias.local>
2025-10-06 09:58:52 +0500

    components fixes

---
+++ src/components/MainTemp.jsx
@@ -0,0 +1,11 @@
+
+
+function Main() {
+    return (
+        <main>
+            <h3>Fresh Organic Products</h3>
+            <p>Discover our wide range of organic fruits and vegetables, sourced directly from local farms.</p>
+        </main>
+    )
+}
+export default Main;
