77ab28a163a5d7825c447cc2a9b81a909840cd25
dias002 <dias.sun666@gmail.com>
2025-10-08 16:23:46 +0500

    route pages

--- src/components/MainTemp.jsx
+++ src/components/MainTemp.jsx
@@ -1,9 +1,14 @@
-
+import {HashRouter, Routes, Route} from "react-router-dom"
 
 function Main() {
     return (
         <main>
-            
+            <HashRouter>
+                <Routes>
+                    <Route path="/" element={<div>Main</div>} />
+                    <Route path="/about" element={<div>About</div>} />
+                </Routes>
+            </HashRouter>
         </main>
     )
 }
